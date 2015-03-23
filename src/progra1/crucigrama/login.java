75f4b3c10f042da670d9a29a3d882daf647d9f4a
Jorge Soler <jorgesoler@me.com>
2015-03-23 15:49:18 -0600

    First Commit
    First

---
+++ src/progra1/crucigrama/login.java
@@ -0,0 +1,54 @@
+package progra1.crucigrama;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+
+public class login extends JFrame{   
+    
+        
+        JButton btnEnter;
+        JLabel text1;   
+        JTextField inputText;
+       
+        
+    public login(){ 
+        super ("login");                                                                        
+        setLayout(null);
+        
+        
+        /*tamaño de la ventana y el centrado de la ventana. Si se pone el valor setSize en la clase main, que vendria siendo login.setSize();,
+        no funciona bien con el paquete Toolkit y Dimension, que son los que centran la ventana*/
+        setSize(300,190); 
+        Toolkit toolkit = getToolkit();
+        Dimension centerLogin = toolkit.getScreenSize();
+        setLocation(centerLogin.width/2 - getWidth()/2, centerLogin.height/2 - getHeight()/2);
+        
+        text1 = new JLabel("Ingrese su nombre"); 
+        Dimension sizeText = text1.getPreferredSize();              /*propiedad de posicionamiento en la ventana*/
+        text1.setBounds(95, 30, sizeText.width, sizeText.height);   /*posicionamiento en la ventana horizontal y vertical*/
+        add(text1);                                                                             
+        
+        
+        inputText = new JTextField(15);                             /*el número define el largo del textField*/
+        Dimension sizeTextField = inputText.getPreferredSize();
+        inputText.setBounds(65, 60, sizeTextField.width, sizeTextField.height);
+        add(inputText);                                                                         
+        inputText.setToolTipText("Es necesario su nombre para poder entrar");
+        
+        
+        btnEnter = new JButton("Aceptar");
+        Dimension sizeButton = btnEnter.getPreferredSize();
+        btnEnter.setBounds(108, 100, sizeButton.width, sizeButton.height);
+        add(btnEnter);   
+
+        
+    }
+    
+    }
+    
+
---
+++ src/progra1/crucigrama/main.java
@@ -0,0 +1,28 @@
+package progra1.crucigrama;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+public class main {
+    
+    
+    public static void main(String[] args) {
+        
+        final login login = new login();                            /*se inicializa las ventana login y sus propiedades*/
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); /*cierra el programa al salirse de la ventana*/
+        login.setVisible(true);
+        login.setResizable(false);
+        
+        login.btnEnter.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e) {
+            login.inputText.getText();
+            new crosswordWindow().setVisible(true);
+            login.dispose();
+            }
+        });
+    
+        
+    }
+    
+}
